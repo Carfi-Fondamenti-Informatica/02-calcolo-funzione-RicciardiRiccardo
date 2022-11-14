539cf8ce8142c8a8e724de79eda170421a1de420
RicciardiRiccardo <117997389+RicciardiRiccardo@users.noreply.github.com>
2022-11-14 17:54:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+   int a;
+   int b;
+   int x;
+   int y;
+   int z;
+   
+   cout << "inserisci i valori di: \n a: \n b: \n x: \n y:";
+   cin >> a;
+   cin >> b;
+   cin >> x;
+   cin >> y;
+   
+if(x < 0, y >0 )
+   z = a*x - b*y;
+   
+  else (x >= 0, y <= 0)
+     z = a*(x*x) - b*y;
+   else
+      z = a*x + b*(y*y);
+   
+   cout << z << endl;
+   
    return 0;
 }
